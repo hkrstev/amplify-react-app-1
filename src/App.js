262cb178ff80fc0c026e8b1ce16ea62aa195c21f
hkrstev <103205287+hkrstev@users.noreply.github.com>
2023-03-13 07:48:39 -0500

    got hardcoded data off and make the app working by the user input....

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+// Import useState and useEffect hooks from React
+import React, { useState, useEffect } from 'react'
+
+// Import the API category from AWS Amplify
+import { API } from 'aws-amplify'
+
 import './App.css';
 
 function App() {
+  // Create coins variable and set to empty array
+  const [coins, updateCoins] = useState([])
+  const [input, updateInput] = useState({ limit: 5, start: 0 })
+
+  function updateInputValues(type, value) {
+    updateInput({ ...input, [type]: value })
+  }
+
+  async function fetchCoins() {
+    const { limit, start } = input
+    const data = await API.get('cryptoapi', `/coins?limit=${limit}&start=${start}`)
+    updateCoins(data.coins)
+  }
+
+  // Define function to all API
+
+  // Call fetchCoins function when component loads
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+  onChange={e => updateInputValues('limit', e.target.value)}
+  placeholder="limit"
+/>
+<input
+  placeholder="start"
+  onChange={e => updateInputValues('start', e.target.value)}
+/>
+
+<button onClick={fetchCoins}>Fetch Coins</button>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
 
-export default App;
+export default App
